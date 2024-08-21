7b3c7adcbeb73ed20c95db133998e29a77c7368b
bivouac95 <tivan0408@gmail.com>
2024-08-21 16:50:21 +0900

    files block

--- src/components/block.jsx
+++ src/components/block.jsx
@@ -4,27 +4,61 @@ import "./block.css";
 
 import Image from "./small_image";
 
-const Block = ({block}) => {
+function format(string) {
+  const date = new Date(string);
+  const options = {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  };
+  return date.toLocaleDateString("ru", options);
+}
 
-  return (
-    <div className="block">
-      <header className="block__header">
-        <h3>{block.headling}</h3>
-      </header>
-      <div className="line"></div>
-      <main className="block__main">
-        <div className="block__images">
-          {block.images.map((image) => (
-            <Image link={image} key={image}/>
-          ))}
+const Block = ({ block }) => {
+  switch (block.page) {
+    case 1:
+      return (
+        <div className="block">
+          <header className="block__header">
+            <h3>{block.headling}</h3>
+          </header>
+          <div className="line"></div>
+          <main className="block__main">
+            <div className="block__images">
+              {block.images.map((image) => (
+                <Image link={image} key={image} />
+              ))}
+            </div>
+            <p className="block__text">{block.content}</p>
+          </main>
+          <div className="block__read-more" onClick={() => {}}>
+            Читать далее
+          </div>
         </div>
-        <p className="block__text">{block.content}</p>
-      </main>
-      <div className="block__read-more" onClick={()=> {}}>
-        Читать далее
-      </div>
-    </div>
-  );
+      );
+
+    case 2:
+      return (
+        <div className="block">
+          <header className="block__header">
+            <h3>{block.headling}</h3>
+          </header>
+          <div className="line"></div>
+          <main className="block__main">
+            <p className="ui">{format(block.date)}</p>
+          </main>
+          <div className="block__read-more" onClick={() => {}}>
+            <div className="block__read-more ui">Скачать</div>
+            <button
+              className="ui_button"
+              onClick={() => window.open(block.content, "_blank")}
+            >
+              <img src="./page/download.svg" alt="" />
+            </button>
+          </div>
+        </div>
+      );
+  }
 };
 
 export default Block;
