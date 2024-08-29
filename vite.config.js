83574ba1d1d95d04498b52437251654891f44474
bivouac95 <tivan0408@gmail.com>
2024-08-29 15:38:45 +0900

    deploy

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: "docs", // Изменяем папку для сборки проекта
-  },
-  base: "/bivouac95/elib/", // Заменяйте на свое имя пользователя и имя репозитория
+  base: "/elib/", // Заменяйте на свое имя пользователя и имя репозитория
 });
